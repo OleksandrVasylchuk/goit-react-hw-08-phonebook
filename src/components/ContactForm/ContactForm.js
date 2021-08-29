4349d9778a13f567e1f6f9e422c24f28a75c21ce
OleksandrVasylchuk <73894374+OleksandrVasylchuk@users.noreply.github.com>
2021-08-29 20:52:54 +0300

    .

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import * as operations from "../../redux/contacts/contacts-operations";
+import { getContacts } from "../../redux/contacts/contacts-selectors";
+import s from "./ContactForm.module.css";
+
+export default function ContactForm() {
+  const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+
+  const onSubmit = (name, number) =>
+    dispatch(operations.addContact({ name, number }));
+
+  const newContact = () => {
+    const includeName = contacts.reduce(
+      (acc, contact) => [...acc, contact.name],
+      []
+    );
+    const includeNumber = contacts.reduce(
+      (acc, contact) => [...acc, contact.number],
+      []
+    );
+
+    if (name === "" || number === "") {
+      toast.warn("Please enter all fields!");
+      return true;
+    }
+
+    if (includeName.includes(name)) {
+      toast.info(`${name} is already in contacts`);
+      return true;
+    } else if (includeNumber.includes(number)) {
+      toast.info(`${number} is already in contacts`);
+      return true;
+    }
+  };
+
+  const handleChangeName = (event) => {
+    setName(event.currentTarget.value);
+  };
+
+  const handleChangeNumber = (event) => {
+    setNumber(event.currentTarget.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    resetName();
+    resetNumber();
+
+    if (newContact()) {
+      return;
+    }
+
+    onSubmit(name, number);
+  };
+
+  const resetName = () => {
+    setName("");
+  };
+  const resetNumber = () => {
+    setNumber("");
+  };
+
+  return (
+    <form className={s.form} onSubmit={handleSubmit}>
+      <label className={s.label}>
+        Name
+        <input
+          className={s.input}
+          type="text"
+          name="name"
+          value={name}
+          placeholder="Eden Clements"
+          onChange={handleChangeName}
+        />
+      </label>
+
+      <label className={s.label}>
+        Number
+        <input
+          className={s.input}
+          type="tel"
+          name="number"
+          value={number}
+          placeholder="000-00-00"
+          onChange={handleChangeNumber}
+          pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
+        />
+      </label>
+      <button className={s.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+}
---
+++ src/views/HomeView.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const styles = {
+  container: {
+    minHeight: "calc(100vh - 50px)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "1024px",
+    borderRadius: "10px",
+    boxShadow:
+      "inset -3px -3px 8px rgb(0 0 0 / 50%), inset 3px 3px 8px hsl(0deg 0% 100% / 90%), 3px 3px 8px -3px rgb(0 0 0 / 80%)",
+  },
+};
+
+export default function HomeView() {
+  return (
+    <div style={styles.container}>
+      <img
+        style={styles.image}
+        src="https://cdn.pixabay.com/photo/2015/05/28/10/31/hyacinth-787841_1280.jpg"
+        alt="welcome page"
+      />
+      {/* <span role="img" aria-label="Иконка приветствия">
+          💁‍♀️
+        </span> */}
+    </div>
+  );
+}
